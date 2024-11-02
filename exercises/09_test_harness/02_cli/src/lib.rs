ead6e3ba7e9d387077c09ff936e9e362487bc96a
matteo-pallini <matteo.pallini@vortexa.com>
2024-11-02 20:14:54 +0000

    libtest_mimic workflow

--- exercises/09_test_harness/02_cli/src/lib.rs
+++ exercises/09_test_harness/02_cli/src/lib.rs
@@ -1,1 +1,12 @@
 
+//! Write an empty entrypoint for the test target defined in `Cargo.toml`.
+
+
+pub fn add_ten(value: i32) -> i32 {
+    value + 10
+}
+
+#[test]
+fn test_add_ten() {
+    assert_eq!(add_ten(5), 15);
+}
